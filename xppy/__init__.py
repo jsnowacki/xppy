cab174df728babc22ec9f3742c1b422ecba86ee2
Jakub Nowacki <j.s.nowacki@googlemail.com>
2011-11-10 10:58:15 -0500

    Release 0.7.0

--- xppy/__init__.py
+++ xppy/__init__.py
@@ -1,19 +1,3 @@
-'''
-This file is part of XPPy.
-
-XPPy is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as 
-published by the Free Software Foundation, either version 3 of 
-the License, or (at your option) any later version.
-
-XPPy is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with XPPy.  If not, see <http://www.gnu.org/licenses/>.
-'''
 from xppy.parser import *
 __all__ = ['parser', 'utils']
-__version__ = '0.6.6'
+__version__ = '0.7.0'
--- xppy/utils/__init__.py
+++ xppy/utils/__init__.py
@@ -1,17 +1,1 @@
-'''
-This file is part of XPPy.
-
-XPPy is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as 
-published by the Free Software Foundation, either version 3 of 
-the License, or (at your option) any later version.
-
-XPPy is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with XPPy.  If not, see <http://www.gnu.org/licenses/>.
-'''
 __all__ = ['allinfo', 'diagram', 'solution', 'data', 'output', 'plot']
